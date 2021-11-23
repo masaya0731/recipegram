c777a04df26623332365cfd92fa4dbc6f3de4022
EC2 Default User <ec2-user@ip-172-31-70-86.ec2.internal>
2021-11-23 14:35:59 +0000

    Add recipe

---
+++ app/models/recipe.rb
@@ -0,0 +1,5 @@
+class Recipe < ApplicationRecord
+
+  belongs_to :user
+  attachment :image
+end
--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = '7c09b7cb53d2d5a72247ae0dd52e1b332f4d72f5fb6bbfc9d729318e718fb8590dca2f2b240c7d19a580cc60a83b9112a586804b7799d7128bae2e9c58fbe057'
